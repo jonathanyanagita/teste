b29f1e11a59786c43561c290282bb7100805e619
Jonathan Yanagita <jonathanyanagita@gmail.com>
2024-05-04 15:18:24 -0300

    User entity and resource

---
+++ src/main/java/com/secao23/projetoSecao23/ProjetoSecao23Application.java
@@ -0,0 +1,13 @@
+package com.secao23.projetoSecao23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSecao23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSecao23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/secao23/projetoSecao23/ProjetoSecao23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secao23.projetoSecao23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSecao23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
