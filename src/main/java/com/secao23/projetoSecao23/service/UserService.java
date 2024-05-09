87263b4ef8947685816d4b00495f918094855875
Jonathan Yanagita <jonathanyanagita@gmail.com>
2024-05-09 20:05:53 -0300

    Feat: Exception handling from update

--- src/main/java/com/secao23/projetoSecao23/service/UserService.java
+++ src/main/java/com/secao23/projetoSecao23/service/UserService.java
@@ -4,6 +4,7 @@ import com.secao23.projetoSecao23.entities.User;
 import com.secao23.projetoSecao23.repositories.UserRepository;
 import com.secao23.projetoSecao23.service.exceptions.DatabaseException;
 import com.secao23.projetoSecao23.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
