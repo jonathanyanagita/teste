5d0109a0a52e1a15c50d552a7f98626e41f61a01
Jonathan Yanagita <jonathanyanagita@gmail.com>
2024-05-09 16:21:30 -0300

    Feat: User update

--- src/main/java/com/secao23/projetoSecao23/resources/UserResource.java
+++ src/main/java/com/secao23/projetoSecao23/resources/UserResource.java
@@ -44,4 +44,10 @@ public class UserResource {
 		//codigo 204
 	}
 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id,obj);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
